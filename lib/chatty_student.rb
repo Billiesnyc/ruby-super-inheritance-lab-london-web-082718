ec9cff97511248e62c1c220ce2b4e1aa17cfb3b9
Billie Simmons <billiesnyc@gmail.com>
2018-09-05 08:48:23 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,15 @@ class ChattyStudent < Student
   
   def raise_hand
     super 
-    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+    puts "Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n 
+    Pick me!\n"
   end
 end
